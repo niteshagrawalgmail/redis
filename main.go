a4d23ab431b882838bef15a5e0fac5e31d2cfb1c
Nitesh Agrawal <nit.agrawal@sap.com>
2021-12-21 15:16:34 +0530

    added logging statement

--- main.go
+++ main.go
@@ -30,17 +30,20 @@
 //
 //}
 
-
 package main
 
 import (
 	"context"
 	"fmt"
-	"github.com/go-redis/redis/v8"
+	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
 )
+
 var ctx = context.Background()
 var client redis.UniversalClient
+
 func main() {
 	storeDataInRedis := http.HandlerFunc(storeData)
 	retriveDataFromRedis := http.HandlerFunc(retriveData)
@@ -49,8 +52,8 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func retriveData(w http.ResponseWriter, r *http.Request){
-	k:= getKeyToRetrive(r)
+func retriveData(w http.ResponseWriter, r *http.Request) {
+	k := getKeyToRetrive(r)
 	v := getDataFromredis(k)
 
 	w.WriteHeader(200)
@@ -60,20 +63,21 @@ func retriveData(w http.ResponseWriter, r *http.Request){
 func storeData(w http.ResponseWriter, r *http.Request) {
 
 	k, v := getKeyValToStore(r)
-	persistDataInRedis(k,v)
+	log.Printf("Got the key:%v and the value:%v to store", k, v)
+	persistDataInRedis(k, v)
 
 	w.WriteHeader(200)
 	fmt.Fprintf(w, "Data stored in redis")
 
 }
 
-func getKeyToRetrive(r *http.Request) (string){
+func getKeyToRetrive(r *http.Request) string {
 	query := r.URL.Query()
 	key := query["key"][0]
 	return key
 }
 
-func getKeyValToStore(r *http.Request) (string, string){
+func getKeyValToStore(r *http.Request) (string, string) {
 	query := r.URL.Query()
 	key := query["key"][0]
 	value := query["value"][0]
@@ -81,32 +85,55 @@ func getKeyValToStore(r *http.Request) (string, string){
 	return key, value
 }
 
+func persistDataInRedis(k string, v string) {
+	redisClient := getRedisClient()
+	pingErr := redisClient.Ping(ctx).Err()
 
+	if pingErr != nil {
+		log.Println("Error occurred while pinging Redis")
+		panic(pingErr)
+	}
 
-func persistDataInRedis(k string, v string){
-		err := getRedisClient().Set(ctx, k, v, 0).Err()
-		if err != nil {
-			panic(err)
-		}
+	log.Println("Successfully pinged Redis")
+
+	fmt.Printf("Setting key %v and value %v in redis", k, v)
+	err := redisClient.Set(ctx, k, v, 0).Err()
+	if err != nil {
+		log.Printf("Error occurred while setting key %v and value %v in redis", k, v)
+		panic(err)
+	}
+
+	log.Printf("Successfully store key %v and value %v in redis", k, v)
+
 }
 
 func getDataFromredis(k string) string {
-	value, err := getRedisClient().Get(ctx,k).Result()
-	if err != nil{
+
+	redisClient := getRedisClient()
+	pingErr := redisClient.Ping(ctx).Err()
+
+	if pingErr != nil {
+		log.Println("Error occurred while pinging Redis")
+		panic(pingErr)
+	}
+	value, err := redisClient.Get(ctx, k).Result()
+	if err != nil {
+		log.Printf("Error occurred while getting key %v from redis", k)
 		panic(err)
 	}
+	log.Printf("Successfully got key %v from redis", k)
 	return value
 }
 
-func getRedisClient() redis.UniversalClient{
-	if client != nil{
+func getRedisClient() redis.UniversalClient {
+	if client != nil {
+		log.Println("Redis client exists. Using the same")
 		return client
-	}else{
+	} else {
 		client = redis.NewUniversalClient(&redis.UniversalOptions{
-			Addrs: []string{"rfs-redisfailover:26379"},
-			MasterName: "mymaster",
+			Addrs: []string{"redis.default.svc.cluster.local:6379"},
 		})
-
+		log.Println("Creating a new Redis Client and using it")
 		return client
 	}
 }
